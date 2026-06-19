a765384da3960103c25c5b4b74fe785e45019e30
High-Petrushka <daniilprokopovic12@gmail.com>
2026-06-19 05:20:27 +0300

    Adding app.rlang.online to the allowed hosts file

--- api/CoreRoot/settings.py
+++ api/CoreRoot/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
-ALLOWED_HOSTS = ["remote-language-app-production.up.railway.app"]
+ALLOWED_HOSTS = ["remote-language-app-production.up.railway.app", "app.rlang.online"]
 
 
 # Application definition
